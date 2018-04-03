00bee28dd0eeecb671a361e975c2d77a85af45d0
Jingyu Yao <yjy.emails@gmail.com>
2018-04-03 12:59:47 -0400

    takes command line args

--- src/main/scala/cs4240/Main.scala
+++ src/main/scala/cs4240/Main.scala
@@ -2,9 +2,16 @@ package cs4240
 
 object Main {
 
+  // fh-bigquery:reddit_comments.2009
   def main(args: Array[String]): Unit = {
-    val testTable = "fh-bigquery:reddit_comments.2009"
-    BigQueryImporter.run(testTable)
-//    CommentAnalysis.run(testTable)
+    require(args.length == 2)
+    val op = args.head.toLowerCase
+    val table = args(1)
+    println(f"Running $op on $table")
+    op match {
+      case "import" => BigQueryImporter.run(table)
+      case "analyze" => CommentAnalysis.run(table)
+      case _ => throw new IllegalArgumentException("unsupported op")
+    }
   }
 }
